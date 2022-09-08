1dda8f00df0bcb1f289597e72bebb178d3ca86c7
Dhanna Riyantka Putra <Dhanna217203@gmail.com>
2022-09-08 20:39:06 +0700

    Project TMS - Main

---
+++ src/test/java/com/juaracoding/PageObjectTMS/Staff/HooksTMSAStaff.java
@@ -0,0 +1,70 @@
+package com.juaracoding.PageObjectTMS.Staff;
+
+import java.io.IOException;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+import com.juaracoding.PageObjectTMS.Drivers.DriverSingleton;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import Utils.Constants;
+import Utils.TestScenario;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import Utils.Utils;
+
+public class HooksTMSAStaff {
+	public static WebDriver driver;
+	public static ExtentTest extentTest;
+	public static ExtentReports reports = new ExtentReports("target/extent-reports.html");
+
+	@Before
+	public void setUP() {
+		DriverSingleton.getInstance(Constants.FIREFOX);
+		driver = DriverSingleton.getDriver();
+		TestScenario[] tests = TestScenario.values();
+		extentTest = reports.startTest(tests[Utils.testCount].getTestName());
+		Utils.testCount++;
+	}
+	
+	@AfterStep
+	public void getResultStatus(Scenario scenario) throws IOException {
+		if (scenario.isFailed()) {
+			String scrennshotpath = Utils.getScreenshot(driver, scenario.getName().replace(" ", "_"));
+			extentTest.log(LogStatus.FAIL, scenario.getName()+"\n"+extentTest.addScreenCapture(scrennshotpath));
+		}
+	}
+	
+	@After
+	public void ex() {
+		reports.endTest(extentTest);
+		reports.flush();
+	}
+	@AfterAll
+	public static void CloseBrowser() {
+		delay(5);
+		DriverSingleton.CloseObjectInstance();
+	}
+
+	static void delay(int detik) {
+		try {
+			Thread.sleep(1000 * detik);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
+
+	}
+
+	static void scroll(int vertical) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scroll(0," + vertical + ")");
+	}
+
+}
